3032bf492cfdbc4b42c7f7db8c77f8e6ccdebc1c
Satish Bandaru <satish.bandaru@walmart.com>
2021-06-26 10:36:15 +0530

    Initial Commit

---
+++ Get it Done/Get_it_DoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Get_it_DoneApp.swift
+//  Get it Done
+//
+//  Created by Satish Bandaru on 26/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Get_it_DoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
